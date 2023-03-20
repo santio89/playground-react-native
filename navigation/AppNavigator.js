97acff84e60ae2f2744916b18752457b645a9bc9
santio89 <santiolais1989@gmail.com>
2023-03-20 14:53:05 -0300

    small fix

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -8,8 +8,8 @@ const Stack = createNativeStackNavigator();
 const AppNavigator = () => {
 
     return (
-        <Stack.Navigator initialRouteName='Apps'>
-            <Stack.Screen name='Apps' component={MainMenu} options={{ title: 'APPS | PLAYGROUND', headerShown: false }}/>
+        <Stack.Navigator initialRouteName='AppsHome'>
+            <Stack.Screen name='AppsHome' component={MainMenu} options={{ title: 'APPS | PLAYGROUND', headerShown: false }}/>
             <Stack.Screen name='ToDoList' component={ToDoList} options={{ title: 'TO DO LIST | PLAYGROUND' }}/>
             <Stack.Screen name='MemoGame' component={MemoGame} options={{ title: 'MEMO GAME | PLAYGROUND' }}/>
         </Stack.Navigator>
