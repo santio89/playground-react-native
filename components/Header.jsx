1f276896df4dcd7428c94384b2d544ee07cf0c3a
santio89 <santiolais1989@gmail.com>
2023-03-30 15:32:21 -0300

    starting to add login

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,20 @@
-import { Text, View, StyleSheet, TouchableWithoutFeedback, Keyboard, Platform } from 'react-native';
+import { Text, View, StyleSheet, TouchableWithoutFeedback, TouchableOpacity, Keyboard, Platform } from 'react-native';
 import Constants from '../constants/Styles';
 import { useSelector } from 'react-redux';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Header() {
   const altColorTheme = useSelector(state => state.settings.altColorTheme.enabled)
 
   return (
-    <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()}>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={[styles.headerContainer, altColorTheme && styles.altBackground, altColorTheme && styles.altBorder]}>
         <Text style={styles.header}>PLAYGROUND</Text>
+        <TouchableOpacity style={[styles.loginContainer, altColorTheme && styles.altLoginContainer]} title="login">
+          <View>
+            <Ionicons name="logo-google" size={28} color={Constants.colorWhite} />
+          </View>
+        </TouchableOpacity>
       </View>
     </TouchableWithoutFeedback>
   )
@@ -17,12 +23,14 @@ export default function Header() {
 
 const styles = StyleSheet.create({
   headerContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     textAlign: 'center',
     backgroundColor: Constants.colorPrimary,
-    padding: 8,
-    paddingTop: Platform.OS === 'web' ? 8 : 20,
+    padding: 10,
+    paddingHorizontal: 10,
+    paddingTop: Platform.OS === 'web' ? 10 : 20,
     width: '100%',
     borderBottomColor: Constants.colorPrimaryDark,
     borderBottomWidth: 1,
@@ -33,6 +41,19 @@ const styles = StyleSheet.create({
     fontSize: Constants.fontLg,
     fontWeight: 'bold',
     fontFamily: Constants.fontPrimaryBold,
+  },
+  loginContainer: {
+    backgroundColor: Constants.colorPrimaryDark,
+    borderRadius: 4,
+    width: 100,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: Constants.colorWhite,
+    borderWidth: 1,
+  },
+  altLoginContainer: {
+    backgroundColor: Constants.colorSecondaryDark,
   },
   /* for alt color theme */
   altBackground: {
