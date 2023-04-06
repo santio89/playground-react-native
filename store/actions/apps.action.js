b6b2607879743ff6ba955a67045c210cb3f04a6c
santio89 <santiolais1989@gmail.com>
2023-04-06 12:37:21 -0300

    adding tokens to fetch requests

--- store/actions/apps.action.js
+++ store/actions/apps.action.js
@@ -92,7 +92,9 @@ export const getAppsData = (userId, storageGetItem) => {
                 const response = await fetch(`${URL_API}apps/${userId}.json?auth=${userId}`)
 
                 const data = await response.json()
-                
+                if (data && !data.toDoList){
+                    data.toDoList = []
+                }
                 data && dispatch({
                     type: GET_APPS_DATA,
                     appsData: data
