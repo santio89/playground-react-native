c944da7210a2259cb10bae2d135883b79cbc64f6
santio89 <santiolais1989@gmail.com>
2023-04-19 14:45:19 -0300

    small fix

--- components/CalcKeyboard.jsx
+++ components/CalcKeyboard.jsx
@@ -20,6 +20,10 @@ const CalcKeyboard = () => {
     }
 
     const handleOperationPress = (btnVal) => {
+        if ((result === Infinity || isNaN(result))){
+            return
+        }
+
         setOperation(btnVal)
         if (result) {
             setSecondNumber(result > 999999999999 ? result?.toExponential(2) : result)
@@ -72,7 +76,7 @@ const CalcKeyboard = () => {
 
     const firstNumberDisplay = () => {
         if (result !== null) {
-            return <Text style={[styles.screenFirstNumber, { fontFamily: Constants.fontPrimaryBold, color: altColorTheme ? Constants.colorSecondaryDark : Constants.colorPrimaryDark }, result.toString().length > 7 && { fontSize: 40 }, result.toString().length > 10 && { fontSize: 34 }, result.toString().length > 12 && { fontSize: 30 }, result.toString().length > 14 && { fontSize: 28 }, result > 999999999999 && { fontSize: 40 }]}>{result > 999999999999 ? result?.toExponential(2).toLocaleString('en-US', 8) : result?.toLocaleString('en-US', 8)}</Text>;
+            return <Text style={[styles.screenFirstNumber, { fontFamily: Constants.fontPrimaryBold, color: altColorTheme ? Constants.colorSecondaryDark : Constants.colorPrimaryDark }, result.toString().length > 7 && { fontSize: 40 }, result.toString().length > 10 && { fontSize: 34 }, result.toString().length > 12 && { fontSize: 30 }, result.toString().length > 14 && { fontSize: 28 }, result > 999999999999 && { fontSize: 40 }]}>{result === Infinity || isNaN(result) ? "ERROR":(result > 999999999999 ? result?.toExponential(2).toLocaleString('en-US', 8) : result?.toLocaleString('en-US', 8))}</Text>;
         }
         if (firstNumber && firstNumber.length < 7) {
             return <Text style={styles.screenFirstNumber}>{firstNumber}</Text>;
