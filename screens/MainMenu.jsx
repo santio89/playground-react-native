bb7ab66d48544d20b7150eeb81d276bd0dd03dc6
santio89 <santiolais1989@gmail.com>
2023-04-25 19:06:25 -0300

    small fix

--- screens/MainMenu.jsx
+++ screens/MainMenu.jsx
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux'
 import Constants from '../constants/Styles'
 import { LANGS } from '../constants/Langs'
 import Header from '../components/Header'
-import { LinearGradient } from 'expo-linear-gradient';
 
 
 const MainMenu = ({ navigation }) => {
@@ -47,26 +46,17 @@ const MainMenu = ({ navigation }) => {
                 <View style={[styles.menuContainer, !darkMode && styles.backgroundWhite, { flexDirection: windowWidth > 800 ? 'row' : 'column' }]}>
                     <View style={{ flex: 1, width: '100%', justifyContent: windowWidth > 800 ? 'center' : 'flex-end', alignItems: windowWidth > 800 ? 'stretch' : 'center' }}>
                         <TouchableOpacity style={[styles.menuOption, altColorTheme && styles.altMenuOption, { width: windowWidth > 800 ? 'auto' : (windowWidth > 320 ? 300 : '100%') }]} onPress={() => { navigation.navigate("ToDoList") }}>
-                         {/*    <LinearGradient
-                                colors={altColorTheme ? [Constants.colorSecondaryDark, Constants.colorSecondary, Constants.colorSecondaryDark] : [Constants.colorPrimaryDark, Constants.colorPrimary, Constants.colorPrimaryDark]}
-                                start={{ x: 0, y: 0 }}
-                                end={{ x: -0.5, y: -0.5 }}
-                                style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, borderRadius: 4, zIndex: -1 }}
-                            /> */}
                             <Text style={[styles.menuOptionText]}>{text.toDoList}</Text>
                         </TouchableOpacity>
                         <TouchableOpacity style={[styles.menuOption, altColorTheme && styles.altMenuOption, { width: windowWidth > 800 ? 'auto' : (windowWidth > 320 ? 300 : '100%') }]} onPress={() => { navigation.navigate("MemoGame") }}>
-
                             <Text style={[styles.menuOptionText]}>{text.memoGame}</Text>
                         </TouchableOpacity>
                     </View>
                     <View style={{ flex: 1, width: '100%', justifyContent: windowWidth > 800 ? 'center' : 'flex-start', alignItems: windowWidth > 800 ? 'stretch' : 'center' }}>
                         <TouchableOpacity style={[styles.menuOption, altColorTheme && styles.altMenuOption, { width: windowWidth > 800 ? 'auto' : (windowWidth > 320 ? 300 : '100%') }]} onPress={() => { navigation.navigate("Weather") }}>
-
                             <Text style={[styles.menuOptionText]}>{text.weather}</Text>
                         </TouchableOpacity>
                         <TouchableOpacity style={[styles.menuOption, altColorTheme && styles.altMenuOption, { width: windowWidth > 800 ? 'auto' : (windowWidth > 320 ? 300 : '100%') }]} onPress={() => { navigation.navigate("Calculator") }}>
-
                             <Text style={[styles.menuOptionText]}>{text.calculator}</Text>
                         </TouchableOpacity>
                     </View>
