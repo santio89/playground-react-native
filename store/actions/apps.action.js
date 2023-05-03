662bf4be66349076449cdd302b2ed5b113ba3c08
santio89 <santiolais1989@gmail.com>
2023-05-03 20:58:32 -0300

    small fix

--- store/actions/apps.action.js
+++ store/actions/apps.action.js
@@ -135,7 +135,7 @@ export const getAppsData = (userId, storageGetItem) => {
                 const response = await fetch(`${URL_API}apps/${userId}.json?auth=${userId}`)
 
                 const data = await response.json()
-            
+                
                 if (data && !data.toDoList) {
                     data.toDoList = { 'items': [] }
                 }
