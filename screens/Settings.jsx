6f325f95c8654ccd98a7d32cdbd87676a92def49
santio89 <santiolais1989@gmail.com>
2023-05-14 18:51:39 -0300

    small fix

--- screens/Settings.jsx
+++ screens/Settings.jsx
@@ -128,7 +128,7 @@ const Settings = ({ navigation }) => {
                         <View style={styles.settingsItem}>
                             <Text style={[styles.settingsItemLabel]}><Text style={[styles.settingsItemIndicator, altColorTheme && styles.altSettingsItemIndicator]}>•&nbsp;</Text><Text>{text.tryMobile}: </Text></Text>
                             <View style={[styles.settingsItemTextWrapper]}>
-                                <TouchableOpacity onPress={() => { Linking.openURL("https://drive.google.com/uc?export=download&id=1SVdkag1ZK5lc8rMLdHhytWvJONkvfLvG") }} style={[styles.settingsItemTextButton, altColorTheme && styles.altSettingsItemTextButton, styles.itemSelected]} >
+                                <TouchableOpacity onPress={() => { Linking.openURL("https://drive.google.com/uc?export=download&id=13Ozg03Sii7LRlDQXpoTyXVIBZHVbytF-") }} style={[styles.settingsItemTextButton, altColorTheme && styles.altSettingsItemTextButton, styles.itemSelected]} >
                                     <LinearGradient
                                         colors={[altColorTheme ? Constants.colorSecondaryDark : Constants.colorPrimaryDark, altColorTheme ? Constants.colorSecondary : Constants.colorPrimary, altColorTheme ? Constants.colorSecondaryDark : Constants.colorPrimaryDark]}
                                         start={{ x: 0, y: 0 }}
