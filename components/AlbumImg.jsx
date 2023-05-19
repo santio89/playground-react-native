4ff680167aa48ecd5ba5a377933bc19f234af1e1
santio89 <santiolais1989@gmail.com>
2023-05-19 12:48:47 -0300

    small fix

--- components/AlbumImg.jsx
+++ components/AlbumImg.jsx
@@ -4,7 +4,7 @@ import { MaterialIcons } from '@expo/vector-icons';
 const AlbumImg = ({ item, index, loading, itemDeleting, handleExchange, exchangeObj, setItemDeleting, setModalVisible, Platform, setModalImg, altColorTheme, Constants, modalVisible }) => {
     return (
         <View style={{ margin: 8, backgroundColor: altColorTheme ? Constants.colorSecondary : Constants.colorPrimary, borderRadius: 8, overflow: 'hidden' }}>
-            <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center', backgroundColor: (itemDeleting === item.id) ? 'gray' : (altColorTheme ? Constants.colorSecondaryDark : Constants.colorPrimaryDark), paddingHorizontal: 2 }}>
+            <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center', backgroundColor: (itemDeleting === item.id) ? 'gray' : (altColorTheme ? Constants.colorSecondaryDark : Constants.colorPrimaryDark), paddingHorizontal: 2, minHeight: 28, maxHeight: 28 }}>
                 <TouchableOpacity disabled={loading} onPress={() => { handleExchange(index) }}>
                     <View style={{ justifyContent: 'center', alignItems: 'center' }}>
                         <MaterialIcons name="swap-vert" size={Constants.fontLg} color={((itemDeleting === item.id)) ? 'dimgray' : (exchangeObj.index1 === index || exchangeObj.index2 === index ? Constants.colorWhite : (altColorTheme ? Constants.colorSecondary : Constants.colorPrimary))} />
